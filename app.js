ba620e003fc51858c2f7c2a98bd446a1fbc205af
Derek Chen <me@derekychen.com>
2016-02-06 14:00:20 -0800

    Working prototype: reads http post requests for lat/lng and updates polyline on dynamic google maps

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
-var http = require("http");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var GoogleMapsAPI = require("googlemaps");
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 
-var GMConfig = {
-  key: 'AIzaSyDKhaT8Jh07EhiDy3n-w9LWiCzwSRXGXJQ',
-  stagger_time:       1000, // for elevationPath 
-  encode_polylines:   false,
-  secure:             true, // use https 
-};
+app.get('/', function (req, res) {
+  res.sendfile('index1.html');
+});
 
-var gmAPI = new GoogleMapsAPI(GMConfig);
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
-var params = {
-  center: '1 Washington Blvd Detroit MI 48226 United States',
-  zoom: 15,
-  size: '500x400',
-  maptype: 'roadmap',
-  style: [
-    {
-      feature: 'road',
-      element: 'all',
-      rules: {
-        hue: '0x00ff00'
-      }
-    }
-  ],
-  path: [
-    {
-      color: '0x0000ff',
-      weight: '5',
-      points: [
-        '42.329638,-83.049796',
-        '42.328496,-83.048937'
-      ]
-    }
-  ]
-};
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+  io.sockets.emit('test',req.body);
+  console.log(req.body);
+});
 
-http.createServer(function (request, response) {
-
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end(gmAPI.staticMap(params));
-}).listen(8080);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8080/');
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
