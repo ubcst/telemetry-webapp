cb451a4170971d85bdd8dac2fb52aad0b3422521
Derek Chen <me@derekychen.com>
2016-03-11 13:24:12 -0800

    Added "save path" button that writes current polyline path to file in /logs.

--- app.js
+++ app.js
@@ -5,26 +5,44 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
+var path = require('path');
+var fs = require('fs');
+
 app.use( bodyParser.json() );
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 http.listen(3000, function () {
-  console.log('UBCST Telemetry app listening on port 3000');
+	console.log('UBCST Telemetry app listening on port 3000');
 });
 
 app.post('/', function (req, res) {
-  res.send('Got a POST request');
-  io.sockets.emit('test',req.body);
-  console.log(req.body);
+	res.send('Got a POST request');
+	io.sockets.emit('test',req.body);
+	console.log(req.body);
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-  	console.log('a user disconnected');
-  });
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('a user disconnected');
+	});
+
+	socket.on('writelog', function(data){
+		var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+		var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().replace(/:/g,"-").slice(0,-5);
+		var filename = localISOTime+'.txt';
+		console.log(filename);
+		var filePath = path.join(__dirname, 'logs', filename);
+		fs.writeFile(filePath, data.pathstring, function(err) {
+		    if(err) {
+		        return console.log('Unable to write file ' + err);
+		    }
+		    console.log('File was saved');
+		});
+	});
 });
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-var path;
+var polyline;
 
 var centerUBC = new google.maps.LatLng(49.2620657,-123.2492408);
 var centerDetroit = new google.maps.LatLng(42.329638,-83.049796);
@@ -21,7 +21,7 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
     var pathCoords = defaultUBC;
-    path = new google.maps.Polyline({
+    polyline = new google.maps.Polyline({
         path: pathCoords,
         geodesic: true,
         strokeColor: '#FF0000',
@@ -29,21 +29,26 @@ function initialize() {
         strokeWeight: 2
     });
 
-    path.setMap(map);
+    polyline.setMap(map);
 }
 
 var socket = io();
 socket.on('test', function(data) {
-    var newpath = path.getPath();
+    var newpath = polyline.getPath();
     newpath.push(new google.maps.LatLng(data.lat,data.lng));
-    path.setPath(newpath);
+    polyline.setPath(newpath);
     document.getElementById("display-latlng").innerHTML = "Last lat/long: " + data.lat + " " + data.lng;
     console.log(data);
-
 });
 
 function clearpath() {
     var newpath = [];
-    path.setPath(newpath);
+    polyline.setPath(newpath);
     console.log("Cleared path data");
+}
+
+function savepath() {
+    var pathstring = polyline.getPath().getArray().toString();
+    socket.emit('writelog',{pathstring});
+    console.log("Trying to write path to log file");
 }
